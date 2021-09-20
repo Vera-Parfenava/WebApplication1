26042ac330ce4bca3b97d7ada8cc47507c5838d5
Vertyachka <veraparfenava@gmail.com>
2021-09-20 05:35:00 +0300

    Cleaning unused libraries

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace WebApplication1.Controllers
 {
