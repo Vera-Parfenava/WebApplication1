3c04eb8d4e02eb0250b3c125caa0fb8c77817b05
Vertyachka <veraparfenava@gmail.com>
2021-09-22 17:20:49 +0300

    Refresh data

--- WebApplication1/Controllers/GuestsController.cs
+++ WebApplication1/Controllers/GuestsController.cs
@@ -3,56 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebApplication1.Models;
+using WebApplication1.Data;
 
 namespace WebApplication1.Controllers
 {
     public class GuestsController : Controller
     {
-        private readonly List<GuestsView> _GuestsViews = new List<GuestsView>
-        {
-            new GuestsView
-            {
-                Id = 1,
-                SurName = "Парфенова",
-                FirstName = "Светлана",
-                Partronymic = "Алексеевна",
-                Age = 48,
-                Side = "невесты",
-                Relation = "мама"
-            },
-            new GuestsView
-            {
-                Id = 2,
-                SurName = "Парфенов",
-                FirstName = "Сергей",
-                Partronymic = "Петрович",
-                Age = 52,
-                Side = "невесты",
-                Relation = "папа"
-            },
-            new GuestsView
-            {
-                Id = 3,
-                SurName = "Парфенов",
-                FirstName = "Дмитрий",
-                Partronymic = "Сергеевич",
-                Age = 21,
-                Side = "невесты",
-                Relation = "брат"
-            },
-            new GuestsView
-            {
-                Id = 4,
-                SurName = "Парфенова",
-                FirstName = "Анастасия",
-                Partronymic = "Сергееевна",
-                Age = 17,
-                Side = "невесты",
-                Relation = "сестра"
-            }
-        };
-
-        public IActionResult Index() => View(_GuestsViews);
+        public IActionResult Index() => View(TestData.GuestsViews);
     }
 }
