da07666b392e0824e854dc245319bbd2824e28a1
Vertyachka <veraparfenava@gmail.com>
2021-09-22 17:48:35 +0300

    Showing the guest card

--- WebApplication1/Controllers/GuestsController.cs
+++ WebApplication1/Controllers/GuestsController.cs
@@ -10,5 +10,15 @@ namespace WebApplication1.Controllers
     public class GuestsController : Controller
     {
         public IActionResult Index() => View(TestData.GuestsViews);
+        public IActionResult Details(int id)
+        {
+            var guest = Data.TestData.GuestsViews.FirstOrDefault(e => e.Id == id);
+            if (guest is null)
+                {
+                return NotFound();
+                }
+            return View(guest);
+        }
     }
 }
+
