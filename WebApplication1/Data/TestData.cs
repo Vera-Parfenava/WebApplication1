d7e2ae6bdf7d9b33b03b19b1bddbc4077073ea55
Vertyachka <veraparfenava@gmail.com>
2021-09-23 15:26:19 +0300

    Summary

--- WebApplication1/Data/TestData.cs
+++ WebApplication1/Data/TestData.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace WebApplication1.Data
 {
     public static class TestData
     {
-        public static List<GuestsView> GuestsViews { get; } = new List<GuestsView>
+        public static  IEnumerable<CollectionEntry> Collections { get; }
+        public static List<GuestsView> GuestsViews { get; set; } = new List<GuestsView>
         {
             new GuestsView
             {
@@ -53,5 +55,24 @@ namespace WebApplication1.Data
             }
         };
 
+        //public static void RemoveGuest (GuestsView guest)
+        //{
+        //    GuestsViews.Remove(guest);
+        //}
+
+        //public static void EditGuest(GuestsView guest)
+        //{
+        //    guest.SurName = Console.ReadLine();
+        //    guest.FirstName = Console.ReadLine();
+        //    guest.Partronymic= Console.ReadLine();
+        //    _ = int.TryParse(Console.ReadLine(), out int age);
+        //    guest.Age = age;
+        //    guest.Side = Console.ReadLine();
+        //    guest.Relation = Console.ReadLine();
+        //}
+        //public static void SaveAction()
+        //{
+        //    GuestsViews.Remove(guest);
+        //}
     }
 }
