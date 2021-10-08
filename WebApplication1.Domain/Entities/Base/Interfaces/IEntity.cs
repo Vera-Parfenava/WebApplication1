a2ccfb29db55397f03bfddf869a1c6aa1fd307d6
Vera-Parfenava <veraparfenava@gmail.com>
2021-10-08 19:13:12 +0300

    The base entities interfaces of subject area

---
+++ WebApplication1.Domain/Entities/Base/Interfaces/IEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Domain.Entities.Base.Interfaces
+{
+    public interface IEntity //Cущность
+    {
+        int Id { get; }
+    }
+    //public interface INamedEntity: IEntity //Именованная сущность
+    //{
+    //    string Name { get; }
+    //}
+    //public interface IOrderedEntity:IEntity //Упорядоченная сущность
+    //{
+    //    int Order { get; }
+    //}
+}
