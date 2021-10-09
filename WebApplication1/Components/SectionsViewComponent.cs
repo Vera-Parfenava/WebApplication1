e7a11d8e81d752fb867ffc7535f187a78ee673c3
Vera-Parfenava <veraparfenava@gmail.com>
2021-10-09 15:16:18 +0300

    Visial components paterns

---
+++ WebApplication1/Components/SectionsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Components
+{
+    public class BrandViewComponent:ViewComponent
+    {
+        // public async Task<IViewComponentResult> InvokeAsync() => View();
+        public IViewComponentResult Invoke() => View();
+
+    }
+}
