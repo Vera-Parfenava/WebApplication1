adda69d0b8961b631327894683d4b9d2dd498080
Vera-Parfenava <veraparfenava@gmail.com>
2021-10-09 17:58:45 +0300

    definition of goods

---
+++ WebApplication1.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Domain.Entities
+{
+    class Product
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
