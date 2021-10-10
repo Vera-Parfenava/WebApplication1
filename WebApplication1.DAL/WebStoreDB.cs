68b63735bc76f4ec5f96908a57aeaf0f36ced495
Vera-Parfenava <veraparfenava@gmail.com>
2021-10-10 20:53:57 +0300

    Defining the BD context

---
+++ WebApplication1.DAL/WebStoreDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using WebApplication1.Domain.Entities;
+
+us
+
+namespace WebApplication1.DAL
+{
+    public class WebStoreDB: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+
+        public WebStoreDB(DbContextOptions<WebStoreDB> options): base(options) { }
+
+        //protected override void OnModelCreating(ModelBuilder model)
+        //{
+        //    base.OnModelCreating(model);
+
+        //    /*model.Entity<Section>;*/
+        //}
+    }
+}
