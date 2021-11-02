911f687775075d5e6e5c1a9e632f019aabfe48e6
Vera-Parfenava <veraparfenava@gmail.com>
2021-11-02 16:41:23 +0300

    Add manager Identity

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Domain.Entities.Identity;
 
 namespace WebApplication1.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SingInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SingInManager)
+        {
+
+        }
+
         public IActionResult Register() => View();
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
