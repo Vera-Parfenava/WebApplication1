e0eb8a8f73254d1999976586cd026ceed4d767ef
Vera-Parfenava <veraparfenava@gmail.com>
2021-11-03 14:44:21 +0300

    Preparation of the registration method for new user

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication1.Domain.Entities.Identity;
+using WebApplication1.ViewModel.Identity;
 
 namespace WebApplication1.Controllers
 {
@@ -18,7 +19,13 @@ namespace WebApplication1.Controllers
 
         }
 
-        public IActionResult Register() => View();
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost]
+        public IActionResult Register(RegisterUserViewModel Model)
+            {
+            return RedirectToAction("Index", "Home");
+             }
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
         public IActionResult AccesDenied() => View();
